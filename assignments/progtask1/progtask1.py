89a09275c29e6edcb14260c35072f2eba78a0678
Dinar5150 <d.yakupov@innopolis.university>
2024-10-04 18:07:38 +0300

    Fix input processor, add tableau generation

--- assignments/progtask1/progtask1.py
+++ assignments/progtask1/progtask1.py
@@ -2,43 +2,59 @@
 
 C = list(map(float, input().split()))
 
-tableMin = [C] # Initialize a table
+A = []
 
-while True: # Read a matrix til the epsilon is given
-    a = list(map(float, input().split()))
-    tableMin.append(a)
-    if len(a) == 1:
-        eps = tableMin[-1]
-        tableMin.pop()
-        b = tableMin[-1]
-        tableMin.pop()
+eps = 0.001 # Default epsilon value
+
+while True: # Read a matrix until the epsilon is given
+    inputVector = list(map(float, input().split()))
+    A.append(inputVector)
+    if len(inputVector) <= 1 and len(A) > 2:
+        if len(inputVector) == 1: eps = A.pop()[0]
+        else: A.pop()
+
+        b = A.pop()
         break
 
-for i in range(len(tableMin[1])-len(tableMin[0])): # Fill the first row with 0's
-    tableMin[0].append(float(0))
+# Print the initial problem function
+def print_problem(is_max_problem):
+    print(f"{"max" if is_max_problem else "min"} z = ", end="") # Target function line
 
-for i in range(len(tableMin)): # Build the table
-    tableMin[i].append(b[i]) # Put the right values into the table
-    if len(tableMin[i]) != len(tableMin[0]):
-        print("The method is not applicable!") # Check that constraints are of equal length
-        exit(0)
+    print(f"{C[0]} * x1", end="")
+    for i in range(1, len(C)):
+        print(f" {"+" if C[i] >= 0 else "-"} {abs(C[i])} * x{i+1}", end="")
 
-def hasBasicVariable(table, row):
-    for i in range(len(table[0])-1):
-        if table[row][i] == 1:
-            for j in range(len(table)):
-                if j == row:
-                    continue
-                if table[j][i] != 0:
-                    break
+    print("\nsubject to the constraints:", end="") # Constraints lines
+    for i in range(len(A)):
+        print("\n\t", end="")
+
+        print(f"{A[i][0]} * x1", end="")
+        for j in range(1, len(A[i])):
+            print(f" {"+" if A[i][j] >= 0 else "-"} {abs(A[i][j])} * x{j+1}", end="")
+
+        print(f" <= {b[i]}", end="")
+
+print_problem(True)
+
+# Function to form a table
+def form_tableau(is_max_problem):
+    if is_max_problem:
+        tableau = [[-i for i in C]]
+    else:
+        tableau = [C[:]] # Copy C into tableau in order to avoid modifications to C
+
+    for i in range(len(A) + 1):
+        tableau[0].append(0.0)
+
+    for i in range(len(A)):
+        tableau.append(A[i][:]) # Copy A into tableau in order to avoid modifications to A
+        for j in range(len(C), len(C) + len(A)):
+            if j == len(A[0]) + i:
+                tableau[i + 1].append(1.0)
             else:
-                return True
-    return False
+                tableau[i+1].append(0.0)
+        tableau[i+1].append(b[i])
 
-for i in range(1, len(tableMin)): # Check that each constraint has a basic variable
-    if not hasBasicVariable(tableMin, i):
-        print("The method is not applicable!")
-        exit(0)
+    return tableau
 
-tableMax = tableMin[:]
-tableMax[0] = [-i for i in tableMin[0]] # Create a table for maximization problem
+form_tableau(True)
