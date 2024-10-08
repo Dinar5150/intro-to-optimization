4858dc8e2f6dd61dbdbeb4d7e0310cb0906763b8
ub3rch <v.molchanov@innopolis.university>
2024-10-08 23:13:01 +0300

    simplex finished, but not working properly

--- assignments/progtask1/progtask1.py
+++ assignments/progtask1/progtask1.py
@@ -10,17 +10,16 @@
 
 # Print the initial problem function
 def print_problem(C,A,b,flag):
-    print(f"{"max" if flag else "min"} z = ", end="") # Target function line
-
+    # Target function line
+    print("z = ",end="")
     print(f"{C[0]} * x1", end="")
     for i in range(1, len(C)):
         print(f" {"+" if C[i] >= 0 else "-"} {abs(C[i])} * x{i+1}", end="")
+    print(f"->{"max" if flag else "min"}",end="\n")
 
-    print("\nsubject to the constraints:", end="") # Constraints lines
+    print("subject to the constraints:", end="") # Constraints lines
     for i in range(len(A)):
-        print("\n\t", end="")
-
-        print(f"{A[i][0]} * x1", end="")
+        print(f"\n\t{A[i][0]} * x1", end="")
         for j in range(1, len(A[i])):
             print(f" {"+" if A[i][j] >= 0 else "-"} {abs(A[i][j])} * x{j+1}", end="")
 
@@ -57,23 +56,28 @@ def check_dimension_correctness():
 
 def minimal(list):
     min=0
-    for i in range(list):
-        if list[min]<list[i]: min = i
+    for i in range(len(list)):
+        if list[i]<list[min]: min = i
     if list[min]<0: return min
     else: return -1
 
-def relations(tableu, ind):
+def relations(tableau, ind):
     min=100000
-    indVar=0
-    for i in range(len(tableu)):
-        if tableu[i][ind]<=0: continue
-        x=tableu[i][len(tableu[i])]/tableu[i][ind]
+    out=0
+    for i in range(1,len(tableau)):
+        if tableau[i][ind]<=0: continue
+        x=tableau[i][-1]/tableau[i][ind]
         if 0<x<min: 
-            indVar=i
+            out=i
             min=x
-    if indVar==0:
-        return -1
-    else return
+    return out
+
+def iterate(tableau, en, out):
+    for i in range(len(tableau)):
+        if i==out:continue
+        k=tableau[i][en]/tableau[out][en]
+        for j in range(len(tableau[i])):
+            tableau[i][j]=tableau[i][j]-k*tableau[out][j]
 
 def simplex(C,A,b,eps,flag):
     # printing problem
@@ -83,17 +87,59 @@ def simplex(C,A,b,eps,flag):
     tableau=form_tableau(C,A,b,flag)
 
     # applying simplex
-    min = minimal(tableau[0])
-    while min!=-1:
-        ind = relations(tableau,min)
+    en = minimal(tableau[0])
+    while en!=-1:
+        out = relations(tableau,en)
+        if out==0:
+            print("Unboundness!\nSimplex is not applicable\n")
+            return ["unbounded",[],[]]
+        else:
+            iterate(tableau, en, out)
 
-        min = minimal(tableau[0])
+        en = minimal(tableau[0])
+    if flag==False: tableau[0][-1]=-1*tableau[0][-1]
+    return ['solved',tableau[0][0:len(C):],tableau[0][-1]]
 
 eps_def = 0.001 # Default epsilon value
 
 # first test
+print("first test")
 C=[3,9]
 A=[ [1,4],
     [1,2]]
 b=[8,4]
-simplex(C,A,b,eps_def,True)
+print(simplex(C,A,b,eps_def,True))
+
+# second test
+print("\nsecond test")
+C=[2,4]
+A=[ [1,2],
+    [1,1]]
+b=[5,4]
+print(simplex(C,A,b,eps_def,True))
+
+# third test
+print("\nthird test")
+C=[2,1]
+A=[ [1,-1],
+    [2,0]]
+print(simplex(C,A,b,eps_def,True))
+b=[8,4]
+
+# fourth test
+print("\nfourth test")
+C=[9,10,16]
+A=[ [18,15,12],
+    [6,4,8],
+    [5,3,3]]
+b=[360,192,180]
+print(simplex(C,A,b,eps_def,True))
+
+# fifth test
+print("\nfifth test")
+C=[-2,2,-6]
+A=[ [2,1,-2],
+    [1,2,4],
+    [1,-1,2]]
+b=[24,23,10]
+print(simplex(C,A,b,eps_def,False))
