5ef7bcc7e507ec77036944d4a0e19593b52d56d0
Dinar5150 <d.yakupov@innopolis.university>
2024-11-02 21:26:06 +0300

    Fix outputs for min case

--- assignments/progtask2/progtask2.py
+++ assignments/progtask2/progtask2.py
@@ -6,6 +6,7 @@ MAX_ITERS = 10**8
 
 # Function to print the initial problem
 def print_problem(C, A, b, is_max_problem):
+
     # Target function line
     print(f"{"max" if is_max_problem else "min"} z = ", end = "")  # Target function line
     print(f"{C[0]} * x1", end = "")
@@ -35,7 +36,7 @@ def generate_initial_point(A, b, eps = EPS_DEF):
     slacks_offset = len(A[0]) - len(A)
     for i in range(MAX_ITERS):
         random_x = np.random.uniform(low = 0 + eps, high = 1 - eps, size = slacks_offset)
-        random_x = np.hstack((random_x, np.zeros(slacks_offset)))
+        random_x = np.hstack((random_x, np.zeros(len(A[0]) - slacks_offset)))
         offset = np.dot(A, random_x)
         b_offset = np.subtract(b, offset)
         x = np.hstack((random_x[:slacks_offset], b_offset))
@@ -46,6 +47,9 @@ def interior_point(is_max_problem, C, A, b, eps = EPS_DEF, x = None):
 
     # Print the initial problem
     print_problem(C, A, b, is_max_problem)
+
+    if not is_max_problem:
+        C = [-i for i in C]
 
     if not check_correctness(C, A, b):
         print("Method not applicable!")
@@ -74,10 +78,6 @@ def interior_point(is_max_problem, C, A, b, eps = EPS_DEF, x = None):
         I = np.eye(len(C))
         AlAlt = np.dot(Al, np.transpose(Al))
 
-        if np.any(AlAlt == float('inf')):
-            print("unbounded")
-            return
-
         AlAlt_inv = np.linalg.inv(AlAlt)
         Alt_AlAlt_inv = np.dot(np.transpose(Al), AlAlt_inv)
         P = np.subtract(I, np.dot(Alt_AlAlt_inv, Al))
@@ -104,19 +104,19 @@ def interior_point(is_max_problem, C, A, b, eps = EPS_DEF, x = None):
 def perform_tests():
     print(f"ALPHA = {ALPHA}", end = "\n\n")
     print("=== FIRST TEST ===")
-    C = [2,1]
-    A = [[1,-1],
-         [2,0]]
-    b=[8,4]
+    C = [1, 4]
+    A = [[2, 1],
+         [3, 5],
+         [1, 3]]
+    b = [3, 9, 5]
     interior_point(True, C, A, b)
 
     print("\n=== SECOND TEST ===")
-    C = [4, 1, 3, 5]
-    A = [[-4, 6, 5, 4],
-        [-3, -2, 4, 1],
-        [-8, -3, 3, 2]]
-    b = [20, 10, 20]
-    interior_point(True, C, A, b, EPS_DEF, [1, 1, 1, 1, 9, 10, 26])
+    C = [-4, 2]
+    A = [[6, 2],
+         [3, -2]]
+    b = [18, 6]
+    interior_point(True, C, A, b, EPS_DEF)
 
     print("\n=== THIRD TEST ===")
     C = [3, 2, 5]
